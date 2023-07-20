5cc1f91fed408b5af5e290d11919a7543809a3e9
Bruno Martins <bgcngm@gmail.com>
2023-07-20 14:33:30 +0100

    manaus: overlay: Enable smooth display with 144Hz
    There's no point to keep using custom pref to set minimum refresh rate
    when AOSP smooth display feature behaves in the exact same way in
    our case.
    
    Change-Id: I497918a9c9687443a9438fb6291769f6e51d9379

--- device.mk
+++ device.mk
@@ -176,7 +176,8 @@ PRODUCT_COPY_FILES += \
 PRODUCT_ENFORCE_RRO_TARGETS := *
 
 PRODUCT_PACKAGES += \
-    FrameworkResOverlayManaus
+    FrameworkResOverlayManaus \
+    SettingsResOverlayManaus
 
 # Power
 PRODUCT_PACKAGES += \
