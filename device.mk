dd28fc23af8fc397ff6ef5c8e30251137209ab03
AdarshGrewal <adarshgrewal@gmail.com>
2024-06-11 11:24:54 +0530

    fuji: Provide liboemcrypto linker config
    Change-Id: Ib7c242be5db626ba0151bbf60dd6d60d7590bc46

--- device.mk
+++ device.mk
@@ -191,6 +191,10 @@ PRODUCT_COPY_FILES += \
 PRODUCT_VENDOR_LINKER_CONFIG_FRAGMENTS += \
     $(LOCAL_PATH)/configs/linker.config.json
 
+# Linker
+PRODUCT_VENDOR_LINKER_CONFIG_FRAGMENTS += \
+    $(LOCAL_PATH)/configs/linker.config.json
+
 # Keymint
 PRODUCT_PACKAGES += \
     libkeymint.vendor \
