da0734cd90faf731d1d6fbd0be6ce87ec3e60a63
bengris32 <bengris32@protonmail.ch>
2025-07-02 00:03:00 -0500

    manaus: Configure GNSS stack

--- device.mk
+++ device.mk
@@ -110,6 +110,10 @@ PRODUCT_PACKAGES += \
     android.hardware.gatekeeper@1.0-impl \
     android.hardware.gatekeeper@1.0-service
 
+# GNSS
+PRODUCT_COPY_FILES += \
+    frameworks/native/data/etc/android.hardware.location.gps.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.location.gps.xml
+
 # Partitions
 PRODUCT_USE_DYNAMIC_PARTITIONS := true
 
