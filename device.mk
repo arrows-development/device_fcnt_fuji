49314dcf559314832a0014dcbb74676b0f70c628
Marc Bourgoin <themard85@gmail.com>
2025-07-02 21:24:40 -0500

    manaus: Import MotCamera4
    * Add com.motorola.camera3 to aux privapp
    
    Co-authored-by: Sarthak Roy <sarthakroy2002@gmail.com>
    Change-Id: Id83083408e9a6adc59466b6db1926cbc7ba3d2c7

--- device.mk
+++ device.mk
@@ -100,6 +100,9 @@ PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.camera.full.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.camera.full.xml \
     frameworks/native/data/etc/android.hardware.camera.raw.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.camera.raw.xml
 
+# Dex-pre-opt exclusions
+$(call add-product-dex-preopt-module-config,MotoSignatureApp,disable)
+
 # Display
 PRODUCT_PACKAGES += \
     android.hardware.graphics.composer@2.1-service \
