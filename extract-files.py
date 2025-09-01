f4c28b7181ed24e6161ca153411b24c721b8ed09
rio004 <riyon336@gmail.com>
2025-09-02 00:39:12 +0900

    fuji: Bump NDK version to fix checkelf
    Co-authored-by: rio004 <riyon336@gmail.com>
    Co-authored-by: claxten10 <claxten10@gmail.com>

--- extract-files.py
+++ extract-files.py
@@ -96,6 +96,72 @@ blob_fixups: blob_fixups_user_type = {
         .regex_replace('DEFAULT_OFFHOST_ROUTE=0x01', 'DEFAULT_OFFHOST_ROUTE=0xC0')
         .regex_replace('OFFHOST_ROUTE_ESE={01}', 'OFFHOST_ROUTE_ESE={C0}')
         .add_line_if_missing('DEFAULT_NFCF_ROUTE=0xC0'),
+    'vendor/bin/hw/android.hardware.biometrics.fingerprint-service.fpc': blob_fixup()
+        .replace_needed('android.hardware.biometrics.common-V3-ndk.so', 'android.hardware.biometrics.common-V4-ndk.so')
+        .replace_needed('android.hardware.biometrics.fingerprint-V3-ndk.so', 'android.hardware.biometrics.fingerprint-V4-ndk.so'),
+    'vendor/lib64/mt6897/libmtkcam_hal_aidl_common.so': blob_fixup()
+        .replace_needed('android.hardware.camera.common-V2-ndk.so', 'android.hardware.camera.common-V1-ndk.so'),
+    'vendor/lib64/libbluetooth_audio_session_aidl.so': blob_fixup()
+        .replace_needed('android.hardware.bluetooth.audio-V3-ndk.so', 'android.hardware.bluetooth.audio-V5-ndk.so'),
+    'vendor/lib64/vendor.mediatek.hardware.bluetooth.audio-V1-ndk.so': blob_fixup()
+        .replace_needed('android.hardware.audio.common-V1-ndk.so', 'android.hardware.audio.common-V2-ndk.so'),
+    'vendor/lib64/mt6897/libpqconfig.so': blob_fixup()
+        .replace_needed('android.hardware.sensors-V2-ndk.so', 'android.hardware.sensors-V3-ndk.so'),
+    'vendor/lib64/hw/audio.primary.mediatek.so': blob_fixup()
+        .replace_needed('android.hardware.audio.common-V1-ndk.so', 'android.hardware.audio.common-V2-ndk.so'),
+    ('vendor/lib64/mt6897/lib3a.ae.stat.so',
+     'vendor/lib64/libarmnn_ndk.mtk.vndk.so'): blob_fixup()
+        .add_needed('liblog.so'),
+    ('vendor/bin/hw/mt6897/android.hardware.graphics.allocator-V2-service-mediatek.mt6897',
+     'vendor/lib64/egl/mt6897/libGLES_mali.so',
+     'vendor/lib64/hw/mt6897/android.hardware.graphics.allocator-V2-mediatek.so',
+     'vendor/lib64/hw/mt6897/android.hardware.graphics.mapper@4.0-impl-mediatek.so',
+     'vendor/lib64/hw/mapper.mediatek.so',
+     'vendor/lib64/libaimemc.so',
+     'vendor/lib64/libcodec2_fsr.so',
+     'vendor/lib64/libcodec2_vpp_AIMEMC_plugin.so',
+     'vendor/lib64/libcodec2_vpp_AISR_plugin.so',
+     'vendor/lib64/libmtkcam_grallocutils.so',
+     'vendor/lib64/libmtkcam_grallocutils_aidlv1helper.so',
+     'vendor/lib64/vendor.mediatek.hardware.camera.isphal-V1-ndk.so',
+     'vendor/lib64/vendor.mediatek.hardware.pq_aidl-V2-ndk.so',
+     'vendor/lib64/vendor.mediatek.hardware.pq_aidl-V3-ndk.so',
+     'vendor/lib64/vendor.mediatek.hardware.pq_aidl-V4-ndk.so',
+     'vendor/lib64/vendor.mediatek.hardware.pq_aidl-V7-ndk.so'): blob_fixup()
+        .replace_needed('android.hardware.graphics.common-V4-ndk.so', 'android.hardware.graphics.common-V6-ndk.so')
+        .replace_needed('android.hardware.graphics.allocator-V1-ndk.so', 'android.hardware.graphics.allocator-V2-ndk.so'),
+
+    'vendor/lib64/mt6897/libmtkcam_hal_aidl_common.so': blob_fixup()
+        .replace_needed('android.hardware.camera.common-V2-ndk.so', 'android.hardware.camera.common-V1-ndk.so'),
+
+    ('vendor/lib64/mt6897/libmtkcam_grallocutils.so',
+     'vendor/lib64/libmtkcam_grallocutils_aidlv1helper.so'): blob_fixup()
+        .replace_needed('android.hardware.graphics.allocator-V1-ndk.so', 'android.hardware.graphics.allocator-V2-ndk.so')
+        .replace_needed('android.hardware.graphics.common-V4-ndk.so', 'android.hardware.graphics.common-V6-ndk.so'),
+
+    ('odm/lib64/libmt_mitee.so',
+     'vendor/bin/hw/android.hardware.security.keymint@3.0-service.mitee'): blob_fixup()
+        .replace_needed('android.hardware.security.keymint-V3-ndk.so', 'android.hardware.security.keymint-V3-ndk-v34.so'),
+
+    'vendor/lib64/mt6897/libpqconfig.so': blob_fixup()
+        .replace_needed('android.hardware.sensors-V2-ndk.so', 'android.hardware.sensors-V3-ndk.so'),
+
+    ('odm/lib64/libTrueSight.so',
+     'odm/lib64/libalLDC.so',
+     'odm/lib64/libalAILDC.so',
+     'odm/lib64/libalhLDC.so',
+     'vendor/lib64/libMiVideoFilter.so',
+     'vendor/lib64/mt6897/libneuralnetworks_sl_driver_mtk_prebuilt.so'): blob_fixup()
+        .clear_symbol_version('AHardwareBuffer_allocate')
+        .clear_symbol_version('AHardwareBuffer_createFromHandle')
+        .clear_symbol_version('AHardwareBuffer_describe')
+        .clear_symbol_version('AHardwareBuffer_getNativeHandle')
+        .clear_symbol_version('AHardwareBuffer_lock')
+        .clear_symbol_version('AHardwareBuffer_lockPlanes')
+        .clear_symbol_version('AHardwareBuffer_release')
+        .clear_symbol_version('AHardwareBuffer_unlock'),
+    'vendor/lib64/libcamera2ndk_vendor.so': blob_fixup()
+        .replace_needed('android.frameworks.cameraservice.device-V1-ndk.so', 'android.frameworks.cameraservice.device-V3-ndk.so'),
 }  # fmt: skip
 
 module = ExtractUtilsModule(
