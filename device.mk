901eddbce62d11d118f54b0dc000fe132c5cb075
kailua <kailua@project2by2.jp>
2025-09-25 01:05:36 +0900

    fuji: overlay: Add regulatory info for M08

--- device.mk
+++ device.mk
@@ -239,6 +239,9 @@ PRODUCT_PACKAGES += \
     EuiccOverlay \
     TelephonyOverlayFuji
 
+PRODUCT_PACKAGES += \
+    RegulatoryInfoOverlayM08
+
 # Power
 PRODUCT_PACKAGES += \
     android.hardware.power-service.pixel-libperfmgr \
