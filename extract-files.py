553f7b2c218ad57ed0ddc8176ff23ea4e499a6eb
Marc Bourgoin <themard85@gmail.com>
2025-10-01 18:37:15 +0900

    fuji: Import MotCamera5 and its deps from V2VH35.58-32-11

--- extract-files.py
+++ extract-files.py
@@ -132,6 +132,9 @@ blob_fixups: blob_fixups_user_type = {
 
     'vendor/etc/init/thermal-mediatek.rc': blob_fixup()
         .regex_replace('android.hardware.thermal-service.mediatek', 'android.hardware.thermal-service.mediatek.fuji'),
+
+    'system_ext/lib64/libgpud_sys.so': blob_fixup()
+        .replace_needed('android.hardware.graphics.common-V5-ndk.so', 'android.hardware.graphics.common-V6-ndk.so'),
 }  # fmt: skip
 
 module = ExtractUtilsModule(
