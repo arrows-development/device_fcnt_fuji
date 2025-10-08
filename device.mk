5262b227606c5df8103298abb8927021c0ca948c
kailua <kailua@project2by2.jp>
2025-10-08 22:00:46 +0900

    fuji: Switch to Lineage libperfmgr

--- device.mk
+++ device.mk
@@ -258,11 +258,12 @@ PRODUCT_PACKAGES += \
 
 # Power
 PRODUCT_PACKAGES += \
-    android.hardware.power-service.pixel-libperfmgr \
+    android.hardware.power-service.lineage-libperfmgr \
     libpowerhalwrap_vendor
 
 PRODUCT_PACKAGES += \
-    libmtkperf_client_vendor
+    libmtkperf_client_vendor \
+    libmtkperf_client
 
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/powerhint.json:$(TARGET_COPY_OUT_VENDOR)/etc/powerhint.json
@@ -303,7 +304,8 @@ PRODUCT_SOONG_NAMESPACES += \
     hardware/motorola \
     hardware/mediatek/libmtkperf_client \
     hardware/google/pixel \
-    hardware/google/interfaces
+    hardware/google/interfaces \
+    hardware/lineage/interfaces/power-libperfmgr
 
 # USB
 PRODUCT_COPY_FILES += \
