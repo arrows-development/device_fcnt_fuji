7fe543bb4adebe3802582752cdea9c5d04fbf78e
LuK1337 <priv.luk@gmail.com>
2026-07-05 13:12:42 +0900

    fuji: Remove vendor/lineage device framework matrix inclusion

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -57,7 +57,6 @@ TARGET_SCREEN_DENSITY := 450
 DEVICE_MANIFEST_FILE += $(DEVICE_PATH)/configs/vintf/manifest.xml
 DEVICE_MATRIX_FILE += $(DEVICE_PATH)/configs/vintf/compatibility_matrix.xml
 DEVICE_FRAMEWORK_COMPATIBILITY_MATRIX_FILE += \
-    vendor/lineage/config/device_framework_matrix.xml \
     hardware/mediatek/vintf/mediatek_framework_compatibility_matrix.xml \
     $(DEVICE_PATH)/configs/vintf/framework_compatibility_matrix.xml
 
