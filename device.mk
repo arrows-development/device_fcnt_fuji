8da79d63d2a4085b1530859f520cc70b02521f08
rexix01 <sebidk700@gmail.com>
2026-07-05 11:58:57 +0900

    fuji: Set flag for custom usb init

--- device.mk
+++ device.mk
@@ -320,6 +320,8 @@ PRODUCT_PACKAGES += \
 $(call soong_config_set, MOTOROLA_TOUCH, HIGH_TOUCH_POLLING_PATH, /sys/class/touchscreen/primary/interpolation)
 
 # USB
+$(call soong_config_set_bool,mediatek_gadget,use_custom_usb_gadget_rc,true)
+
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.usb.accessory.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.usb.accessory.xml \
     frameworks/native/data/etc/android.hardware.usb.host.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.usb.host.xml
