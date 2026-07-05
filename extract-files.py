1c322ea280b07e30ca429abf6d71dacc10459b54
kailua <kailua@project2by2.jp>
2026-07-05 17:18:49 +0900

    fuji: Fix audio

--- extract-files.py
+++ extract-files.py
@@ -46,6 +46,7 @@ blob_fixups: blob_fixups_user_type = {
         .add_needed('android.hardware.sensors@1.0-convert-shared.so'),
 
     'vendor/lib64/hw/audio.primary.mediatek.so': blob_fixup()
+        .replace_needed('android.hardware.audio.common-V1-ndk.so', 'android.hardware.audio.common-V2-ndk.so')
         .add_needed('libstagefright_foundation-v33.so')
         .replace_needed('libutils.so','libutils-v32.so')
         .replace_needed('libalsautils.so','libalsautils-v31.so')
