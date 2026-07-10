32df10208254d3cc5144cf221148e5b5b8b14668
Mashopy <eliasgheeraert@gmail.com>
2026-07-10 22:19:24 +0900

    fuji: Update blobs dependency on android.hardware.graphics.common-V6 to android.hardware.graphics.common-V7
    Required for Android 16 QPR1.
    
    Change-Id: Ia6621ce9f9a91625bbff7c840b3c101f9779bec2

--- extract-files.py
+++ extract-files.py
@@ -112,7 +112,7 @@ blob_fixups: blob_fixups_user_type = {
      'vendor/lib64/vendor.mediatek.hardware.pq_aidl-V2-ndk.so',
      'vendor/lib64/vendor.mediatek.hardware.pq_aidl-V4-ndk.so',
      'vendor/lib64/vendor.mediatek.hardware.pq_aidl-V7-ndk.so'): blob_fixup()
-        .replace_needed('android.hardware.graphics.common-V4-ndk.so', 'android.hardware.graphics.common-V6-ndk.so')
+        .replace_needed('android.hardware.graphics.common-V4-ndk.so', 'android.hardware.graphics.common-V7-ndk.so')
         .replace_needed('android.hardware.graphics.allocator-V1-ndk.so', 'android.hardware.graphics.allocator-V2-ndk.so'),
 
     'vendor/lib64/mt6897/libneuralnetworks_sl_driver_mtk_prebuilt.so': blob_fixup()
@@ -136,7 +136,7 @@ blob_fixups: blob_fixups_user_type = {
         .regex_replace('android.hardware.thermal-service.mediatek', 'android.hardware.thermal-service.mediatek.fuji'),
 
     'system_ext/lib64/libgpud_sys.so': blob_fixup()
-        .replace_needed('android.hardware.graphics.common-V5-ndk.so', 'android.hardware.graphics.common-V6-ndk.so'),
+        .replace_needed('android.hardware.graphics.common-V5-ndk.so', 'android.hardware.graphics.common-V7-ndk.so'),
 
     ('vendor/lib64/mt6897/libmmlpqImpl.so',
      'vendor/lib64/libpqxmlflagparser.so',
