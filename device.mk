84af78db9c2b4da6136281d8befb5129b89e3c71
Mashopy <eliasgheeraert@gmail.com>
2026-07-29 21:41:55 +0900

    fuji: Inherit MSSI common overlays

--- device.mk
+++ device.mk
@@ -249,6 +249,8 @@ PRODUCT_COPY_FILES += \
 # Overlays
 PRODUCT_ENFORCE_RRO_TARGETS := *
 
+$(call inherit-product, hardware/mediatek/overlay/mssi.mk)
+
 PRODUCT_PACKAGES += \
     FrameworkResOverlayFuji \
     SettingsResOverlayFuji \
