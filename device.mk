bb6b84570477fa275077130be078a3712fd9eab8
Mashopy <eliasgheeraert@gmail.com>
2026-07-29 22:07:23 +0900

    fuji: Build stub mediatek-common framework
    Change-Id: If9661170a653c3bed11332b9f92fd6bf85e184bb

--- device.mk
+++ device.mk
@@ -269,6 +269,7 @@ TARGET_BOARD_PLATFORM := mt6897
 
 # Radio
 $(call inherit-product, hardware/lineage/compat/frameworks/compat.mk)
+$(call inherit-product, hardware/mediatek/frameworks/mediatek-frameworks.mk)
 
 PRODUCT_PACKAGES += \
     vendor_mdota_symlink
