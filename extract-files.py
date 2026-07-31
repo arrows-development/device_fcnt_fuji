dc4166d9baaea18a40ede19c9731c0df2c9e6498
ItsVixano <giovanniricca@proton.me>
2026-07-31 18:56:37 +0900

    fuji: Rename libsink library to not conflict with the rust module
    Patch libimsma to load libsink-mtk
    Also rename libsink.so lib file

--- extract-files.py
+++ extract-files.py
@@ -39,7 +39,7 @@ blob_fixups: blob_fixups_user_type = {
         .apktool_patch('ims-patches'),
     ('system_ext/etc/init/init.vtservice.rc', 'vendor/etc/init/android.hardware.neuralnetworks-shim-service-mtk.rc'): blob_fixup()
         .regex_replace('start', 'enable'),
-    'system_ext/lib64/libsink.so': blob_fixup()
+    'system_ext/lib64/libsink-mtk.so': blob_fixup()
         .add_needed('libaudioclient_shim.so'),
     'system_ext/lib64/libsource.so': blob_fixup()
         .add_needed('libui_shim.so'),
