6eddee65bfa84541e7b9bf4f8b107d42dd556248
kailua <kailua@project2by2.jp>
2026-08-02 21:13:25 +0900

    fuji: Drop unused DT2W leftovers
    * DT2W workaround no longer working at latest kernel, so disable it until we proper handle kernel source

--- device.mk
+++ device.mk
@@ -286,8 +286,6 @@ PRODUCT_PACKAGES += \
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/powerhint.json:$(TARGET_COPY_OUT_VENDOR)/etc/powerhint.json
 
-$(call soong_config_set,power_libperfmgr,mode_extension_lib,//$(LOCAL_PATH):power-ext-fuji)
-
 # Partitions
 PRODUCT_USE_DYNAMIC_PARTITIONS := true
 
